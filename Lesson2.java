c8785780b224616e7cb988c9e9439e27b661c969
Ramil2188 <87470400+Ramil2188@users.noreply.github.com>
2022-01-18 17:57:34 +0500

    Add files via upload

---
+++ Lesson2.java
@@ -0,0 +1,38 @@
+/** 
+Java 1. HomeWork 2
+Student Ramil'
+Data 18.01.2022
+*/
+
+class Lesson2 {
+    public static void main(String[] args) {
+        System.out.println(isBetween10And20(10, -2));
+        System.out.println(isBetween10And20(12, 5));
+        System.out.println(isBetween10And20(20, 1));
+        
+        isPositiveOrNegative(5);
+        isPositiveOrNegative(0);
+        isPositiveOrNegative(-5);
+        
+        System.out.println(isNegative(-5));
+        System.out.println(isNegative(0));
+        System.out.println(isNegative(5));
+        
+        repeatString("Hello, Java!", 3);
+    }
+    static boolean isBetween10And20(int a, int b){
+        return a + b >= 10 && a + b <= 20;
+    }
+    static void isPositiveOrNegative(int a) {
+        System.out.println(a >=0? "Positive" : "Negative");
+    }
+    static boolean isNegative(int a) {
+        return a < 0;
+    }
+    static void repeatString(String s, int count){
+        for (int i = 0; i < count; i++) {
+            System.out.println(s);
+        }
+    }
+    
+}
